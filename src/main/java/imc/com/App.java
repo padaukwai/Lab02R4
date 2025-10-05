b30b0f27088c22b9e8e93f541cd026e106000301
padaukwai <waimarhlaing86@gmail.com>
2025-10-06 00:56:32 +0630

    Function split connect and disconnect. Branch is local develop branch.

--- src/main/java/imc/com/App.java
+++ src/main/java/imc/com/App.java
@@ -5,7 +5,7 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
+    public void connect()
     {
         try
         {
@@ -18,26 +18,17 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 20;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(10000);
+                Thread.sleep(30000);
                 // Connect to database
-                //if you run java on windows
-              //  con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
-                //if you run java on docker compose
-                //db:3306
-                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-               // Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -50,7 +41,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -63,5 +60,72 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+    public static void main(String[] args)
+    {
+//        try
+//        {
+//            // Load Database driver
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//        }
+//        catch (ClassNotFoundException e)
+//        {
+//            System.out.println("Could not load SQL driver");
+//            System.exit(-1);
+//        }
+//
+//        // Connection to the database
+//        Connection con = null;
+//        int retries = 5;
+//        for (int i = 0; i < retries; ++i)
+//        {
+//            System.out.println("Connecting to database...");
+//            try
+//            {
+//                // Wait a bit for db to start
+//                Thread.sleep(50000);
+//                // Connect to database
+//                //if you run java on windows
+//              //  con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
+//                //if you run java on docker compose
+//                //db:3306
+//                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true", "root", "example");
+//                System.out.println("Successfully connected");
+//                // Wait a bit
+//                Thread.sleep(10000);
+//                // Exit for loop
+//                break;
+//            }
+//            catch (SQLException sqle)
+//            {
+//                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+//                System.out.println(sqle.getMessage());
+//            }
+//            catch (InterruptedException ie)
+//            {
+//                System.out.println("Thread interrupted? Should not happen.");
+//            }
+//        }
+//
+//        if (con != null)
+//        {
+//            try
+//            {
+//                // Close connection
+//                con.close();
+//            }
+//            catch (Exception e)
+//            {
+//                System.out.println("Error closing connection to database");
+//            }
+//        }
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
